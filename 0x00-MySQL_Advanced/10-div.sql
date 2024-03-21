2bc21e220d4bc349149c762ed3a5b6f09c8e44e9
Grace O. Nweke <lagra2001@gmail.com>
2024-03-21 22:42:04 +0000

    solving for task 7 to 12

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -1,7 +1,7 @@
 -- a SQL script that creates a function SafeDiv that divides (and returns) the first by the second number or returns 0 if the second number is equal to 0.
 
 CREATE FUNCTION SafeDiv(a INT, b INT)
-RETURNS INT
+RETURNS FLOAT
 BEGIN
     IF b = 0 THEN
         RETURN 0;
