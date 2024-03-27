4fad757339ef6ad74c9006571aa6cc3ba4dc5210
grace.o.nweke <lagra2001@gmail.com>
2024-03-27 21:44:20 +0000

    solving fpr task 3

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -9,6 +9,27 @@ import redis
 from typing import Union, Callable
 
 
+def call_history(method: Callable) -> Callable:
+    """Decorator to store history of inputs and outputs for a function."""
+    @functools.wraps(method)
+    def wrapper(self, *args, **kwargs):
+        inputs_key = method.__qualname__ + ":inputs"
+        outputs_key = method.__qualname__ + ":outputs"
+
+        # Store input arguments
+        self._redis.rpush(inputs_key, str(args))
+
+        # Execute the original function
+        output = method(self, *args, **kwargs)
+
+        # Store output
+        self._redis.rpush(outputs_key, str(output))
+
+        return output
+
+    return wrapper
+
+
 def count_calls(method: Callable) -> Callable:
     """Decorator to count the number of times a method is called."""
     @functools.wraps(method)
@@ -24,6 +45,8 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
