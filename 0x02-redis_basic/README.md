737ea84bad1005342f772931d34c932140b6af63
grace.o.nweke <lagra2001@gmail.com>
2024-03-27 21:20:45 +0000

    solving for exercise 1

---
+++ 0x02-redis_basic/README.md
@@ -0,0 +1,13 @@
+#0x02. Redis basic
+
+## OBJECTIVES
+- Learn how to use redis for basic operations
+- Learn how to use redis as a simple cache
+
+## MANDATORY TASKS
+- exercise.py
+
+## ADVANCED TASKS (Optional Tasks)
+- web.py
+**Any file not seen means I couldn't do that task and seeing a task file does not mean I got all the ALX green checks. Do not copy work, only use as guide**
+
---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+a Python function that returns all students sorted by average score
+"""
+
+import uuid
+import redis
+from typing import Union
+
+
+class Cache:
+    def __init__(self):
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
